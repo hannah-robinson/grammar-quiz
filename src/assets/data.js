55585f6032b71d34ee78259a56d003d77236c005
Hannah Robinson <hannah.robinson.git@gmail.com>
2025-02-07 12:15:49 +1300

    Add grammar questions

---
+++ src/assets/data.js
@@ -0,0 +1,70 @@
+export const data = [
+  {
+    question: 'I&apos;ve been waiting ____ three hours.',
+    option1: 'during',
+    option2: 'for',
+    answer: 'for',
+  },
+  {
+    question: 'I like to go swimming ____ the summer.',
+    option1: 'during',
+    option2: 'for',
+    answer: 'during',
+  },
+  {
+    question: 'In Kyiv, it&apos;s usually very cold during the winter.',
+    option1: 'during',
+    option2: 'for',
+    answer: 'during',
+  },
+  {
+    question: 'Don&apos;t look at your mobile phone ____ dinner!',
+    option1: 'during',
+    option2: 'for',
+    answer: 'during',
+  },
+  {
+    question: 'I&apos;ve been learning English ____ five years.',
+    option1: 'during',
+    option2: 'for',
+    answer: 'for',
+  },
+  {
+    question: 'He has been working out every day ____ six months now',
+    option1: 'during',
+    option2: 'for',
+    answer: 'for',
+  },
+  {
+    question: 'Please don&apos;t talk ____ the film.',
+    option1: 'during',
+    option2: 'for',
+    answer: 'during',
+  },
+  {
+    question: '____ my vacation, I took trip to the Carpathian Mountains.',
+    option1: 'during',
+    option2: 'for',
+    answer: 'for',
+  },
+  {
+    question:
+      'The Carpathian Mountains were so beautiful, but it rained ten times ____ my trip.',
+    option1: 'during',
+    option2: 'for',
+    answer: 'during',
+  },
+  {
+    question:
+      'If you visit Lviv, make sure you try the amazing chocolate ____ your stay.',
+    option1: 'during',
+    option2: 'for',
+    answer: 'during',
+  },
+  {
+    question: 'You should brush your teeth ____ two minutes twice a day.',
+    option1: 'during',
+    option2: 'for',
+    answer: 'for',
+  },
+]
